3c3132109114c2a4ca81e1a1807b2d67bb6098d5
yvonbenahita <yvonbenahita@gmail.com>
2022-08-08 15:55:23 +0300

    Internationalization

--- benawp-add-id-column.php
+++ benawp-add-id-column.php
@@ -11,7 +11,7 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        #
- * Text Domain:       benawp
+ * Text Domain:       benawp-add-id-column
  * Domain Path:       /languages
  */
 
@@ -20,9 +20,10 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+// Get with the filter
 if ( ! function_exists( 'benawp_add_id_column' ) ) :
 	function benawp_add_id_column( $columns ) {
-		$columns['post_id_clmn'] = __( 'ID', 'benawp' );
+		$columns['post_id_clmn'] = __( 'ID', 'benawp-add-id-column' );
 
 		return $columns;
 	}
@@ -31,6 +32,7 @@ if ( ! function_exists( 'benawp_add_id_column' ) ) :
 	add_filter( 'manage_pages_columns', 'benawp_add_id_column', 5 ); // for pages
 endif;
 
+// Print with an action hook
 if ( ! function_exists( 'benawp_column_content' ) ) :
 	function benawp_column_content( $column, $id ) {
 		if ( $column === 'post_id_clmn' ) {
@@ -40,4 +42,13 @@ if ( ! function_exists( 'benawp_column_content' ) ) :
 
 	add_action( 'manage_posts_custom_column', 'benawp_column_content', 5, 2 ); // for posts
 	add_action( 'manage_pages_custom_column', 'benawp_column_content', 5, 2 ); // for pages
+endif;
+
+// Internationalization
+if ( ! function_exists( 'benawp_load_text_domain' ) ) :
+	function benawp_load_text_domain() {
+		load_plugin_textdomain( 'benawp', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	add_action( 'plugin_loaded', 'benawp_load_text_domain' );
 endif;
